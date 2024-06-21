cb26bf1940c7cae5b23af9c83a1f32f86b9b3e7d
Nitin Saxena <144582475+Nitin10cd@users.noreply.github.com>
2024-06-21 19:09:29 +0530

    Create Demi.jsx

---
+++ .github/workflows/Demi.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "/Demi.css"
+import { Outlet } from 'react-router-dom'
+const Demi = () => {
+  return (
+   <>
+   <Outlet/>
+   </>
+  )
+}
+
+export default Demi
