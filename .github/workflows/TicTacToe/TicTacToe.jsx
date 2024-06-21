3c31845ea5117dbc0d571665e20a1c8e9b4ea95d
Nitin Saxena <144582475+Nitin10cd@users.noreply.github.com>
2024-06-21 14:24:52 +0530

    Create TicTacToe.jsx

---
+++ .github/workflows/TicTacToe/TicTacToe.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+import './Tic.css'; // Optional: You can style your components
+import Ticbg from './ticbg.jpg'
+import Tictac from './tictac.jpg'
+import Title1 from './title1.png'
+const TicTacToe = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+  const winner = calculateWinner(board);
+
+  const handleClick = (index) => {
+    if (winner || board[index]) return;
+    const newBoard = [...board];
+    newBoard[index] = xIsNext ? 'X' : 'O';
+    setBoard(newBoard);
+    setXIsNext(!xIsNext);
+  };
+
+  const renderSquare = (index) => {
+    return (
+      <button className="square" onClick={() => handleClick(index)}>
+        {board[index]}
+      </button>
+    );
+  };
+
+  const status = winner
+    ? `Winner: ${winner}`
+    : `Next player: ${xIsNext ? 'X' : 'O'}`;
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setXIsNext(true);
+  };
+
+  return (
+    <div className="TicParent">
+      <div className="game">
+      <img className='titleimg' src={Title1} />
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+      <div  className="status">{status}</div>
+      <button className="reset-button" onClick={resetGame}>
+        Reset Game
+      </button>
+    </div>
+    </div>
+  );
+};
+
+const calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;
+};
+
+export default TicTacToe;
